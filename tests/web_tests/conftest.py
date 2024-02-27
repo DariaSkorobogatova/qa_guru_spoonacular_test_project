6d91bca76be30707a4a272a14f943e2f873b9ae7
Daria Skorobogatova <daria.skorobogatova@yandex.ru>
2024-02-27 18:37:18 +0300

    changed conftest file

--- tests/web_tests/conftest.py
+++ tests/web_tests/conftest.py
@@ -1,55 +1,43 @@
-import os
 import pytest
-from dotenv import load_dotenv
-from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selene.support.shared import config
+from selene import browser
 from spoonacular_test_project.helper import attach_web
+from dotenv import load_dotenv
+import os
 
 
-DEFAULT_BROWSER_VERSION = "121.0"
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        '--browser_version',
-        default='100.0'
-    )
-
-
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope='session', autouse=True)
 def load_env():
     load_dotenv()
 
 
 @pytest.fixture(scope='function', autouse=True)
-def setup_browser(request):
-    browser_version = request.config.getoption('--browser_version')
-    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+def browser_configs():
+    browser.config.base_url = 'https://www.chitai-gorod.ru'
+    config.window_width = 1920
+    config.window_height = 1080
     options = Options()
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": browser_version,
+        "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
         }
     }
     options.capabilities.update(selenoid_capabilities)
-    load_dotenv()
+
     login = os.getenv('LOGIN')
     password = os.getenv('PASSWORD')
-    driver = webdriver.Remote(command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
-                              options=options)
-    browser.config.base_url = "https://spoonacular.com/"
-    browser.config.driver = driver
-    browser.config.driver_options = options
 
-    browser.config.timeout = 5.0
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
 
     yield browser
 
